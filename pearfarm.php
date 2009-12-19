d1bf7b4cd963f8ee351b686027dc78cc06c44230
Alan Pinstein <apinstein@mac.com>
2009-12-19 18:22:12 -0500

    Get basic architecture wired up. Not quite working yet but on the right path.

--- pearfarm.php
+++ pearfarm.php
@@ -30,6 +30,8 @@ class PEARFarm_Specification
         foreach ($files as $f) {
             $this->files[] = $f;
         }
+
+        return $this;
     }
 
     public function addGitFiles($exclude = array())
@@ -39,9 +41,7 @@ class PEARFarm_Specification
         $lastLine = exec('git ls-files', $output, $result);
         if ($result != 0) throw( new Exception("Error ($result) running git ls-files: " . join("\n", $output)) );
 
-        foreach ($output as $file) {
-            $this->files[] = $file;
-        }
+        $this->addFiles($output);
 
         return $this;
     }
@@ -53,15 +53,21 @@ class PEARFarm_Specification
 
     public function __call($name, $value)
     {
-        if (strncmp($name, 'set', 3) === 0)
-        {
-            $varName = substr($name, 3);
-            $varName[0] = strtolower($varName[0]);
-            //if (isset($this->$varName)) // not sure how to test (w/o reflection) to make sure $this->$varName exists.
-            //{
-                $this->$varName = $value;
-                return $this;
-            //}
+        switch (substr($name, 0, 3)) {
+            case 'set':
+                $varName = substr($name, 3);
+                $varName[0] = strtolower($varName[0]);
+                //if (isset($this->$varName)) // not sure how to test (w/o reflection) to make sure $this->$varName exists.
+                //{
+                    $this->$varName = $value;
+                    return $this;
+                //}
+                break;
+            case 'get':
+                $varName = substr($name, 3);
+                $varName[0] = strtolower($varName[0]);
+                return $this->$varName;
+                break;
         }
         throw new Exception("Function $name does not exist.");
     }
@@ -78,46 +84,38 @@ class PEARFarm_Builder
 
     public function build()
     {
+        $spec = $this->spec;
+
         require_once('PEAR/PackageFileManager2.php');
         PEAR::setErrorHandling(PEAR_ERROR_DIE);
         //require_once 'PEAR/Config.php';
         //PEAR_Config::singleton('/path/to/unusualpearconfig.ini');
         // use the above lines if the channel information is not validating
         $packagexml = new PEAR_PackageFileManager2;
-        $e = $packagexml->setOptions(
-        array('baseinstalldir' => 'PhpDocumentor',
-         'packagedirectory' => '/',
-         'ignore' => array('TODO', 'tests/'), // ignore TODO, all files in tests/
-         'installexceptions' => array('phpdoc' => '/*'), // baseinstalldir ="/" for phpdoc
-         'dir_roles' => array('tutorials' => 'doc'),
-         'exceptions' => array('README' => 'doc', // README would be data, now is doc
-                               'PHPLICENSE.txt' => 'doc'))); // same for the license
-        $packagexml->setPackage('MyPackage');
-        $packagexml->setSummary('this is my package');
-        $packagexml->setDescription('this is my package description');
-        $packagexml->setChannel('mychannel.example.com');
-        $packagexml->setAPIVersion('1.0.0');
-        $packagexml->setReleaseVersion('1.2.1');
-        $packagexml->setReleaseStability('stable');
-        $packagexml->setAPIStability('stable');
-        $packagexml->setNotes("We've implemented many new and exciting features");
-        $packagexml->setPackageType('php'); // this is a PEAR-style php script package
-        $packagexml->addRelease(); // set up a release section
-        $packagexml->setOSInstallCondition('windows');
-        $packagexml->addInstallAs('pear-phpdoc.bat', 'phpdoc.bat');
-        $packagexml->addIgnoreToRelease('pear-phpdoc');
-        $packagexml->addRelease(); // add another release section for all other OSes
-        $packagexml->addInstallAs('pear-phpdoc', 'phpdoc');
-        $packagexml->addIgnoreToRelease('pear-phpdoc.bat');
-        $packagexml->addRole('pkg', 'doc'); // add a new role mapping
-        $packagexml->setPhpDep('4.2.0');
-        $packagexml->setPearinstallerDep('1.4.0a12');
-        $packagexml->addMaintainer('lead', 'cellog', 'Greg Beaver', 'cellog@php.net');
-        $packagexml->setLicense('PHP License', 'http://www.php.net/license');
+#        $e = $packagexml->setOptions(
+#            array(
+#             'baseinstalldir' => 'PhpDocumentor',
+#             'packagedirectory' => '/',
+#             'ignore' => array('TODO', 'tests/'), // ignore TODO, all files in tests/
+#             'installexceptions' => array('phpdoc' => '/*'), // baseinstalldir ="/" for phpdoc
+#             'dir_roles' => array('tutorials' => 'doc'),
+#             'exceptions' => array('README' => 'doc', // README would be data, now is doc
+#                                   'PHPLICENSE.txt' => 'doc') // same for the license
+#            )
+#        );
+        $packagexml->setPackage($spec->getName());
+        $packagexml->setChannel($spec->getChannel());
+        $packagexml->setSummary($spec->getSummary());
+        $packagexml->setDescription($spec->getDescription());
+        $packagexml->setReleaseVersion($spec->getReleaseVersion());
+        $packagexml->setReleaseStability($spec->getReleaseStability());
+        $packagexml->setAPIVersion($spec->getApiVersion());
+        $packagexml->setAPIStability($spec->getApiStability());
+        $packagexml->setPhpDep($spec->getDependsOnPHPVersion());
+        $packagexml->setPearinstallerDep($spec->getDependsOnPearInstallerVersion());
+        //$packagexml->addMaintainer('lead', 'cellog', 'Greg Beaver', 'cellog@php.net');
+        $packagexml->setLicense($spec->getLicense());
         $packagexml->generateContents(); // create the <contents> tag
-        // replace @PHP-BIN@ in this file with the path to php executable!  pretty neat
-        $test->addReplacement('pear-phpdoc', 'pear-config', '@PHP-BIN@', 'php_bin');
-        $test->addReplacement('pear-phpdoc.bat', 'pear-config', '@PHP-BIN@', 'php_bin');
         $pkg = &$packagexml->exportCompatiblePackageFile1(); // get a PEAR_PackageFile object
         // note use of {@link debugPackageFile()} - this is VERY important
         if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
--- pearfarm.spec
+++ pearfarm.spec
@@ -1,12 +1,11 @@
 <?php
 
 $spec = PEARFarm_Specification::newSpec()
-                            ->setName('iphp')
-                            ->setChannel('iphp.pearfarm.net')
-                            ->setSummary('PHP Shell')
-                            ->setDescription('An interactive PHP Shell (or Console, or REPL).')
-                            ->setReleaseVersion('1.0.0')
-                            ->setReleaseStability('stable')
-                            ->setLicense('bsd')
-                            ->addGitFiles();
-
+            ->setName('iphp')
+            ->setChannel('iphp.pearfarm.net')
+            ->setSummary('PHP Shell')
+            ->setDescription('An interactive PHP Shell (or Console, or REPL).')
+            ->setReleaseVersion('1.0.0')
+            ->setReleaseStability('stable')
+            ->setLicense('bsd')
+            ->addGitFiles();
