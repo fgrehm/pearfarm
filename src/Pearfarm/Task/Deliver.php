bd2db620ed46063c7a6f34685bbea727563302a9
Alan Pinstein <apinstein@mac.com>
2009-12-24 00:03:57 -0500

    add stub code to deliver package to server

--- src/Pearfarm/Task/Deliver.php
+++ src/Pearfarm/Task/Deliver.php
@@ -1,22 +1,42 @@
 <?php
+/* vim: set expandtab tabstop=2 shiftwidth=2: */
+
 class Pearfarm_Task_Deliver implements Pearfarm_ITask {
-  
+
   public function run($args) {
- 
+    $pkgTgzPath = getcwd() . "/{$args[2]}";
+
+    // read these from config file
+    $apiKey = '247b7825633eb9e485364207c99973a1';
+    $channel = 'joe.dev.pearfarm.org';  // read from pkg
+    $hash = md5(md5_file($pkgTgzPath) . $apiKey);
+
+    $ch = curl_init("http://{$channel}/upload.xml");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+          'file'      => "@{$pkgTgzPath}",
+          'hash'      => $hash
+          )
+        );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $postResult = curl_exec($ch);
+    curl_close($ch);
+    print "$postResult";
+
+    exit(0);
   }
-  
+
   public function showHelp() {
- 
+
   }
-  
+
   public function getName() {
     return "deliver";
   }
-  
+
   public function getAliases() {
     return array();
   }
-  
+
   public function getDescription() {
     return "sends the package to pearfarm.org";
   }
