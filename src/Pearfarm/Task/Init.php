8fd2429d185d27dd3d7a9dec79660bed0768ea5a
Fabio Rehm <fgrehm@gmail.com>
2009-12-28 17:05:46 -0300

    fixing init task that broke on last commit

--- src/Pearfarm/Task/Init.php
+++ src/Pearfarm/Task/Init.php
@@ -1,6 +1,6 @@
 <?php
 /* vim: set expandtab tabstop=2 shiftwidth=2: */
-class Pearfarm_Task_Init extends Pearfarm_Task_Plant {
+class Pearfarm_Task_Init implements Pearfarm_ITask {
 
   public function run($args) {
     if (isset($args[2])) {
@@ -29,5 +29,37 @@ class Pearfarm_Task_Init extends Pearfarm_Task_Plant {
 
   public function getDescription() {
     return "Creates a pearfarm.spec file for your project.";
+  }
+
+  public function basicSpecFile($packageName = NULL) {
+    if ($packageName === NULL) {
+      $packageName = 'TODO: Your package name here';
+    }
+    $creatorName = 'TODO: Your name here';
+    $creatorEmail = 'TODO: Your email here';
+    $user = 'TODO: Your username here';
+    $channel = 'TODO: Release channel here';
+    $summary = 'TODO: One-line summary of your PEAR package';
+    $description = 'TODO: Longer description of your PEAR package';
+
+    return <<<STR
+<?php
+
+\$spec = Pearfarm_PackageSpec::create(array(Pearfarm_PackageSpec::OPT_BASEDIR => dirname(__FILE__)))
+             ->setName('{$packageName}')
+             ->setChannel('{$channel}')
+             ->setSummary('{$summary}')
+             ->setDescription('{$description}')
+             ->setReleaseVersion('0.0.1')
+             ->setReleaseStability('alpha')
+             ->setApiVersion('0.0.1')
+             ->setApiStability('alpha')
+             ->setLicense(Pearfarm_PackageSpec::LICENSE_MIT)
+             ->setNotes('Initial release.')
+             ->addMaintainer('lead', '{$creatorName}', '{$user}', '{$creatorEmail}')
+             ->addGitFiles()
+             ->addExecutable('{$packageName}')
+             ;
+STR;
   }
 }
