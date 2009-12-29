e9bfd232d2a33fea258ff5ac9d6cbb38eceff156
Alan Pinstein <apinstein@mac.com>
2009-12-29 15:24:27 -0500

    get basic vfsStream working

--- test/PackageSpecTest.php
+++ test/PackageSpecTest.php
@@ -1,25 +1,90 @@
 <?php
 /* vim: set expandtab tabstop=2 shiftwidth=2: */
 
-require_once('PHPUnit/Framework.php');
-require_once(dirname(__FILE__) . '/../src/Pearfarm/ITask.php');
-require_once(dirname(__FILE__) . '/../src/Pearfarm/PackageSpec.php');
 /**
 	* Requires vfsstream for filesystem mocking
 	* http://code.google.com/p/bovigo/wiki/vfsStreamDocsInstall
 	*/
 require_once('vfsStream/vfsStream.php');
 
+function makeTree($fileArray, $root = 'root')
+{
+    vfsStreamWrapper::setRoot(new vfsStreamDirectory($root));
+    foreach ($fileArray as $file) {
+      $vfsFile = vfsStream::url($file);
+      $vfsFileDir = vfsStream::url(dirname($file));
+      // containing dir
+      if (!file_exists($vfsFileDir))
+      {
+        $ok = mkdir($vfsFileDir, 0755, true);
+        if (!$ok) die("mkdir $vfsFileDir failed");
+      }
+      // item
+      if (substr($file, -1) === '/')
+      {
+        // dir
+        if (!file_exists($vfsFile))
+        {
+          $ok = mkdir($vfsFile);
+          if (!$ok) die("mkdir $file failed");
+        }
+      }
+      else
+      {
+        // file
+        $vfsFileUrl = vfsStream::url($file);
+        // touch doesn't work with vfs yet
+        $f = fopen($vfsFileUrl, 'r');
+        fclose($f);
+      }
+    }
+}
+
+function showTree($path)
+{
+    // dump path just made
+    print "ls $path:\n";
+    foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator(vfsStream::url($path)), RecursiveIteratorIterator::SELF_FIRST) as $item) {
+      print $item->getPathname();
+      if ($item->isDir())
+      {
+        print "/";
+      }
+      print "\n";
+    }
+    print "DONE\n";
+}
+
+require_once('PHPUnit/Framework.php');
+require_once(dirname(__FILE__) . '/../src/Pearfarm/ITask.php');
+require_once(dirname(__FILE__) . '/../src/Pearfarm/PackageSpec.php');
+
 class PackageSpecTest extends PHPUnit_Framework_TestCase {
 
   public function setUp() {
     $this->spec = new Pearfarm_PackageSpec();
     vfsStreamWrapper::register();
-    $root = new vfsStreamDirectory('root');
-    vfsStreamWrapper::setRoot($root);
+    makeTree(array(
+            'root/file.php',
+            'root/a/a.php',
+            'root/a/b/b.php',
+            'root/a/b/c/c.php',
+            'root/b/',
+            'root/c/d/',
+    ));
+    showTree('root');
+
+    die();
   }
 
   public function testCreatesPearSpec() {
-    $this->markTestIncomplete('need to figureo ut vfsStreamWrapper');
+    $this->markTestIncomplete();
   }
+  public function testAddExcludeFilesRegex() {
+    $this->markTestIncomplete();
+  }
+  public function testAddFilesRegex() {
+    $this->markTestIncomplete();
+  }
+  // etc
 }
